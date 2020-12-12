b1edf1ab64a3642415631e2e11edf5d520922896
729234283 <729234283@qq.com>
2020-12-13 04:38:22 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
 </template>
 
 <style lang="scss">
-* {
-  margin: 0;
-  padding: 0;
+body {
+  width: 100%;
+  height: 100%;
 }
 
 #app {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,18 @@
       <el-menu
         default-active="1-4-1"
         class="el-menu-vertical-demo"
-        @open="handleOpen"
-        @close="handleClose"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+        @open="onOpen"
+        @close="onClose"
         :collapse="isCollapse"
       >
         <el-submenu index="1">
+          <template #title>
+            <i class="el-icon-location"></i>
+            <span>导航一</span>
+          </template>
           <el-menu-item-group>
             <template #title>分组一</template>
             <el-menu-item index="1-1">选项1</el-menu-item>
@@ -31,20 +38,40 @@ export default {
   setup() {
     const isCollapse = ref(false);
 
+    const onOpen = () => {};
+
+    const onClose = () => {};
+
     return {
-      isCollapse
+      isCollapse,
+      onOpen,
+      onClose
     };
   }
 };
 </script>
 
 <style>
+.el-container {
+  height: 100%;
+}
+
+.el-aside {
+  height: 100%;
+  background-color: #545c64;
+  box-shadow: 2px 0 6px rgba(0, 21, 41, 0.35);
+}
+
 .el-header {
-  line-height: 60px;
   background-color: gray;
 }
 
 .el-main {
+  height: 100%;
   background-color: #e9eef3;
+}
+
+.el-menu {
+  overflow: hidden;
 }
 </style>
