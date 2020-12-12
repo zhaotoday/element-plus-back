c1e94529aa32deb279bdec472e1edf52e201b90a
729234283 <729234283@qq.com>
2020-12-13 04:47:39 +0800

    update

---
+++ src/utils/consts/index.js
@@ -0,0 +1,25 @@
+import SidebarMenu from "./sidebar-menu";
+
+// 基础地址
+const BaseUrl = "https://my-app.liruan.cn";
+
+// CDN
+const CdnUrl = "https://my-app.lrcdn.cn";
+
+// 接口地址
+const ApiUrl = BaseUrl + "/admin";
+
+// 公开接口地址
+const PublicApiUrl = BaseUrl + "/public";
+
+// 分页大小
+const PageSize = 10;
+
+export default {
+  SidebarMenu,
+  BaseUrl,
+  CdnUrl,
+  ApiUrl,
+  PublicApiUrl,
+  PageSize
+};
---
+++ src/utils/consts/sidebar-menu.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    icon: "md-cube",
+    name: "products",
+    title: "商品管理",
+    children: [
+      {
+        title: "商品列表",
+        route: "/products/products/list"
+      },
+      {
+        title: "商品分类",
+        route: "/products/products/categories"
+      }
+    ]
+  }
+];
