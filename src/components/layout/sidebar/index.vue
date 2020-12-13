5d198a467be3cfb95ec8a75d6e5923d9ae55b0d2
729234283 <729234283@qq.com>
2020-12-13 10:18:11 +0800

    update

--- src/components/layout/sidebar/index.vue
+++ src/components/layout/sidebar/index.vue
@@ -2,36 +2,11 @@
   <div class="c-sidebar">
     <div class="c-sidebar__logo" @click="$router.push('/')">
       <div class="c-sidebar__logo-icon">
-        <Avatar icon="logo-chrome"></Avatar>
+        <el-avatar icon="el-icon-platform-eleme" size="medium"></el-avatar>
       </div>
       后台管理系统
     </div>
-    <Menu
-      ref="menu"
-      theme="dark"
-      :active-name="activeName"
-      width="auto"
-      :open-names="openNames"
-      @on-select="handleSelect"
-    >
-      <Submenu
-        v-for="(menu1, index1) in $consts.SidebarMenu"
-        :key="index1"
-        :name="menu1.name"
-      >
-        <template slot="title">
-          <Icon :type="menu1.icon" />
-          {{ menu1.title }}
-        </template>
-        <MenuItem
-          v-for="(menu2, index2) in menu1.children"
-          :key="index2"
-          :name="menu2.route"
-        >
-          {{ menu2.title }}
-        </MenuItem>
-      </Submenu>
-    </Menu>
+    abc
   </div>
 </template>
 
